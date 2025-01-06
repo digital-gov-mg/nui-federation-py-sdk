0ff2f52ca9913ca14c37c166b33b58e8d01490ba
Ando <hi@andostronaut.com>
2025-01-06 15:34:53 +0300

    feat: dont use os getenv for base url and api key

--- src/base.py
+++ src/base.py
@@ -1,14 +1,13 @@
-import os
 import requests
 
 
 class Base:
     def __init__(self, base_url=None, api_key=None):
-        self.base_url = base_url or os.getenv("NUI_FED_API_URL")
-        self.api_key = api_key or os.getenv("NUI_FED_API_KEY")
+        self.base_url = base_url
+        self.api_key = api_key
 
         if not self.base_url or not self.api_key:
-            raise ValueError("API URL and API Key must be provided")
+            raise ValueError("Please provide a base URL and an API key")
 
     def request(self, method, endpoint, data=None, params=None):
         url = f"{self.base_url}{endpoint}"
