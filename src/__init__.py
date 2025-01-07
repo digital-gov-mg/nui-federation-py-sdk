ef5b7e2f3362582db8b999f44b8d689bccdccb21
Ando <hi@andostronaut.com>
2025-01-07 14:09:40 +0300

    fix: remove version and author on init

--- src/__init__.py
+++ src/__init__.py
@@ -1,12 +1,6 @@
 from src.features.uin import UIN
 
-__version__ = "0.1.0"
-__author__ = "UGD"
-
 
 class NuiFederation:
     def __init__(self, base_url: str, api_key: str):
         self.uin = UIN(base_url=base_url, api_key=api_key)
-
-    def main(self):
-        print(f"Version: {__version__}")
